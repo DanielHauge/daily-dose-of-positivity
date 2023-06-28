5111a1667bc11633f47106f7cb22877e8a09a8b9
Daniel Hauge <animcuil@gmail.com>
2023-06-28 19:17:36 +0200

    Fix to double init render

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,9 @@ export default function Home() {
 
   const [hour, set_hour] = useState(new Date().getHours())
   setInterval(()=>{
-    if (hour != new Date().getHours()){
-      set_hour(new Date().getHours())
+    const now = new Date().getHours()
+    if (hour !== now){
+      set_hour(now)
     }
   },1000)
   
