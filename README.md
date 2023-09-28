af5139923b17ed64321e6f8d074dd2c0780975ce
Daniel Hauge <animcuil@gmail.com>
2023-09-28 23:27:44 +0200

    Added doc + more words

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 To provide a daily dose of positivity to the world, and to those whom need or want it.
 It is the decider of the day, the one who decides if you will have a good, amazing or excellent day!
 Regardless of the weather, the day of the week, the month or the year, that person being in office, it will always be there for you.
+
+## dev
+
+```sh
+npm run dev
+```
+
+*install next*
+
+```sh
+npm i -g next
+```
--- app/components/word.tsx
+++ app/components/word.tsx
@@ -2,10 +2,12 @@
 
 export default function Word() {
 
-    const positiveWords = ["Awesome", "Great", "Amazing", "Wonderful", "Fantastic", "Super", "Nice", "Good", "Excellent", "Perfect", "Incredible", "Brilliant", "Fabulous", "Terrific", "Outstanding", "Spectacular", "Splendid", "Superb", "Phenomenal", "Stupendous", "Astonishing", "Unbelievable", "Mind-blowing", "Unreal", "Breathtaking", "Impressive", "Remarkable", "Exceptional", "Extraordinary", "Stunning", "Marvellous", "Magnificent", "Bewitching", "Delightful", "Enchanting", "Captivating", "Charming", "Divine", "Gorgeous", "Heavenly", "Lovely", "Ravishing", "Sublime", "Exquisite", "Delicious", "Delectable", "Exquisite", "Astounding", "Miraculous"]
+    const positiveWords = ["Awesome", "Great", "Amazing", "Wonderful", "Fantastic", "Super", "Nice", "Good", "Excellent", "Perfect", "Incredible", "Brilliant", "Fabulous", "Terrific", "Outstanding", "Spectacular", "Splendid", "Superb", "Phenomenal", "Stupendous", "Astonishing", "Unbelievable", "Mind-blowing", "Unreal", "Breathtaking", "Impressive", "Remarkable", "Exceptional", "Extraordinary", "Stunning", "Marvellous", "Magnificent", "Bewitching", "Delightful", "Enchanting", "Captivating", "Charming", "Divine", "Gorgeous", "Heavenly", "Lovely", "Ravishing", "Sublime", "Exquisite", "Delicious", "Delectable", "Exquisite", "Astounding", "Miraculous", "Elegant", "Mumbo Jumbo", "Baronial", "Charismatic", "Chipper", "Dreamy", "Electrifying", "Empowered", "Enticing", "Alluring", "Exhilarating", "Gleaming", "Impeccable", "Flawless", "Irresistible", "Jolly", "Kind-hearted", "Legendary", "Nifty", "Peachy", "Phenomenal", "Rad", "Revolutionary", "Sensational", "Striking", "Vibrant", "Vivacious", "Wondrous", "Xenodochial"]
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     const day = days[new Date().getDay()];
-    const word = positiveWords[new Date().getDate() % positiveWords.length];
+    // Compute day of the year (1-365)
+    const dayOfYear = Math.floor((new Date() - new Date(new Date().getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+    const word = positiveWords[dayOfYear % positiveWords.length];
     const hour = new Date().getHours();
     const time_of_day = hour < 12 ? "morning" : hour < 18 ? "afternoon" : "evening";
 
